f7d52309b5125ea279139b8609b3988111b3fd25
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-18 18:45:40 +0900

    [ADD] Typescript로 React 상태관리하기 - useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Greetings from './Greeting';
+import MyForm from './MyForm';
 
-const App: React.FC = () => { // React.FC 장점 1. 기본적으로 children props가 탑재되어 있음 2. 값들이 자동완성이 됨
-  const onClick = (name: string) => {
-    console.log(name);
-  };
-
+function App() { // React.FC 장점 1. 기본적으로 children props가 탑재되어 있음 2. 값들이 자동완성이 됨
+  const onSubmit = (form: { name: string, description: string }) => {
+    console.log(form);
+  }
   return (
-    <Greetings name="리액트" optional="만나서 반갑습니다." onClick={onClick} />
+    <div>
+      <MyForm onSubmit={onSubmit} />
+    </div>
   );
 }
 
