7b51bff25a1a84680ac3b081bf89dcaa835fb225
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-20 11:01:49 +0900

    [ADD] Typescript와 Context API 활용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
 
 
 function App() { // React.FC 장점 1. 기본적으로 children props가 탑재되어 있음 2. 값들이 자동완성이 됨
   return (
     <div>
-      <ReducerSample />
+      <SampleProvider>
+        <ReducerSample />
+      </SampleProvider>
     </div>
   );
 }
